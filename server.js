a22773bbe3ddb7b913cdc720343a1f3fd9a18b0c
CristianMD03 <ccmdjt@gmail.com>
2026-02-12 13:47:39 -0500

    Update server.js

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 const express = require("express");
 const mysql = require("mysql2");
 const path = require("path");
+const session = require("express-session");
+const cookieParser = require("cookie-parser");
 
 const app = express();
 
